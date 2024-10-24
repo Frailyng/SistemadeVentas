1bf15524f9ce395ab065ef940017a6566be48f2c
frailyng <frailindes@gmail.com>
2024-10-24 17:51:45 -0400

    Modificar Creado y arreglado

--- SistemadeVentas/Services/InventarioService.cs
+++ SistemadeVentas/Services/InventarioService.cs
@@ -3,67 +3,67 @@ using SistemadeVentas.DAL;
 using SistemadeVentas.Models;
 using System.Linq.Expressions;
 
-namespace SistemadeVentas.Services;
-
-public class InventarioService
+namespace SistemadeVentas.Services
 {
-    private readonly Contexto _context;
-
-    public InventarioService(Contexto context)
+    public class InventarioService
     {
-        _context = context;
-    }
-    public async Task<bool> Guardar(Inventario inventario)
-    {
-        //Busca la prioridad, si no existe la inserta, si existe la modifica
-        if (!await Existe(inventario.InventarioId))
-            return await Insertar(inventario);
-        else
-            return await Modificar(inventario);
-    }
+        private readonly Contexto _context;
 
-    public async Task<bool> Insertar(Inventario inventario)
-    {
-        _context.Inventarios.Add(inventario);
-        return await _context.SaveChangesAsync() > 0;
-    }
+        public InventarioService(Contexto contexto)
+        {
+            _context = contexto;
+        }
 
-    public async Task<bool> Modificar(Inventario inventario)
-    {
-        _context.Update(inventario);
-        return await _context.SaveChangesAsync() > 0;
-    }
+        public async Task<bool> Guardar(Inventario inventario)
+        {
+            if (await Existe(inventario.InventarioId))
+                return await Modificar(inventario);
+            else
+                return await Insertar(inventario);
+        }
 
-    public async Task<bool> Existe(int inventarioId)
-    {
-        return await _context.Inventarios
-            .AnyAsync(p => p.InventarioId == inventarioId);
+        public async Task<bool> Insertar(Inventario inventario)
+        {
+            _context.Inventarios.Add(inventario);
+            return await _context.SaveChangesAsync() > 0;
+        }
 
-    }
-
-    public async Task<bool> Eliminar(int id)
-    {
-        var filasEliminadas = await _context.Inventarios
-            .Where(p => p.InventarioId == id)
-            .ExecuteDeleteAsync();
-        return filasEliminadas > 0;
-    }
+        public async Task<bool> Modificar(Inventario inventario)
+        {
+            _context.Update(inventario);
+            return await _context.SaveChangesAsync() > 0;
+        }
 
 
-    public async Task<Inventario?> Buscar(int id)
-    {
-        return await _context.Inventarios
-            .Include(i => i.Producto) 
-            .FirstOrDefaultAsync(i => i.InventarioId == id);
-    }
+        public async Task<bool> Existe(int inventarioId)
+        {
+            return await _context.Inventarios
+                .AnyAsync(p => p.InventarioId == inventarioId);
+        }
 
+        public async Task<bool> Eliminar(int id)
+        {
+            var filasEliminadas = await _context.Inventarios
+                .Where(p => p.InventarioId == id)
+                .ExecuteDeleteAsync();
+            return filasEliminadas > 0;
+        }
 
-    public async Task<List<Inventario>> Listar(Expression<Func<Inventario, bool>> criterio)
-    {
-        return await _context.Inventarios
-            .AsNoTracking()
-            .Include(i => i.Producto)
-            .Where(criterio)
-            .ToListAsync();
+        public async Task<Inventario?> Buscar(int id)
+        {
+            return await _context.Inventarios
+                .Include(i => i.Producto)
+                .FirstOrDefaultAsync(i => i.InventarioId == id);
+        }
+
+        public async Task<List<Inventario>> Listar(Expression<Func<Inventario, bool>> criterio)
+        {
+            return await _context.Inventarios
+                .AsNoTracking()
+                .Include(i => i.Producto)
+                .Where(criterio)
+                .ToListAsync();
+        }
     }
 }
+
