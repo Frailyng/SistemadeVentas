4e001e3471472c571c32a3569963864515f0e955
frailyng <frailindes@gmail.com>
2024-10-24 14:00:31 -0400

    Index Creado

--- SistemadeVentas/Services/ProductosService.cs
+++ SistemadeVentas/Services/ProductosService.cs
@@ -1,7 +1,4 @@
-﻿using System;
-using System.Collections.Generic;
-using System.Threading.Tasks;
-using Microsoft.EntityFrameworkCore;
+﻿using Microsoft.EntityFrameworkCore;
 using SistemadeVentas.DAL;
 using SistemadeVentas.Models;
 
