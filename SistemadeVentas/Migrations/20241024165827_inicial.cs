83be91fcba1d93400e43b2ea6cb1ce81ec698a64
frailyng <frailindes@gmail.com>
2024-10-24 12:59:07 -0400

    Migration y Base de datos Creada

---
+++ SistemadeVentas/Migrations/20241024165827_inicial.cs
@@ -0,0 +1,131 @@
+﻿using System;
+using Microsoft.EntityFrameworkCore.Migrations;
+
+#nullable disable
+
+namespace SistemadeVentas.Migrations
+{
+    /// <inheritdoc />
+    public partial class inicial : Migration
+    {
+        /// <inheritdoc />
+        protected override void Up(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.CreateTable(
+                name: "Facturaciones",
+                columns: table => new
+                {
+                    FacturaId = table.Column<int>(type: "INTEGER", nullable: false)
+                        .Annotation("Sqlite:Autoincrement", true),
+                    NombreCliente = table.Column<string>(type: "TEXT", maxLength: 100, nullable: false),
+                    FechaFactura = table.Column<DateTime>(type: "TEXT", nullable: false),
+                    Total = table.Column<decimal>(type: "TEXT", nullable: false),
+                    MetodoPago = table.Column<string>(type: "TEXT", nullable: false)
+                },
+                constraints: table =>
+                {
+                    table.PrimaryKey("PK_Facturaciones", x => x.FacturaId);
+                });
+
+            migrationBuilder.CreateTable(
+                name: "Productos",
+                columns: table => new
+                {
+                    ProductoId = table.Column<int>(type: "INTEGER", nullable: false)
+                        .Annotation("Sqlite:Autoincrement", true),
+                    Nombre = table.Column<string>(type: "TEXT", maxLength: 100, nullable: false),
+                    Descripcion = table.Column<string>(type: "TEXT", maxLength: 255, nullable: false),
+                    Precio = table.Column<decimal>(type: "TEXT", nullable: false),
+                    Categoria = table.Column<string>(type: "TEXT", nullable: false),
+                    CodigoProducto = table.Column<string>(type: "TEXT", nullable: false),
+                    Proveedor = table.Column<string>(type: "TEXT", nullable: false),
+                    EstaActivo = table.Column<bool>(type: "INTEGER", nullable: false)
+                },
+                constraints: table =>
+                {
+                    table.PrimaryKey("PK_Productos", x => x.ProductoId);
+                });
+
+            migrationBuilder.CreateTable(
+                name: "DetalleFactura",
+                columns: table => new
+                {
+                    DetalleFacturaId = table.Column<int>(type: "INTEGER", nullable: false)
+                        .Annotation("Sqlite:Autoincrement", true),
+                    ProductoId = table.Column<int>(type: "INTEGER", nullable: false),
+                    Cantidad = table.Column<int>(type: "INTEGER", nullable: false),
+                    PrecioUnitario = table.Column<decimal>(type: "TEXT", nullable: false),
+                    Subtotal = table.Column<decimal>(type: "TEXT", nullable: false),
+                    FacturacionFacturaId = table.Column<int>(type: "INTEGER", nullable: true)
+                },
+                constraints: table =>
+                {
+                    table.PrimaryKey("PK_DetalleFactura", x => x.DetalleFacturaId);
+                    table.ForeignKey(
+                        name: "FK_DetalleFactura_Facturaciones_FacturacionFacturaId",
+                        column: x => x.FacturacionFacturaId,
+                        principalTable: "Facturaciones",
+                        principalColumn: "FacturaId");
+                    table.ForeignKey(
+                        name: "FK_DetalleFactura_Productos_ProductoId",
+                        column: x => x.ProductoId,
+                        principalTable: "Productos",
+                        principalColumn: "ProductoId",
+                        onDelete: ReferentialAction.Cascade);
+                });
+
+            migrationBuilder.CreateTable(
+                name: "Inventario",
+                columns: table => new
+                {
+                    InventarioId = table.Column<int>(type: "INTEGER", nullable: false)
+                        .Annotation("Sqlite:Autoincrement", true),
+                    ProductoId = table.Column<int>(type: "INTEGER", nullable: false),
+                    CantidadDisponible = table.Column<int>(type: "INTEGER", nullable: false),
+                    FechaIngreso = table.Column<DateTime>(type: "TEXT", nullable: false),
+                    FechaUltimaActualizacion = table.Column<DateTime>(type: "TEXT", nullable: false)
+                },
+                constraints: table =>
+                {
+                    table.PrimaryKey("PK_Inventario", x => x.InventarioId);
+                    table.ForeignKey(
+                        name: "FK_Inventario_Productos_ProductoId",
+                        column: x => x.ProductoId,
+                        principalTable: "Productos",
+                        principalColumn: "ProductoId",
+                        onDelete: ReferentialAction.Cascade);
+                });
+
+            migrationBuilder.CreateIndex(
+                name: "IX_DetalleFactura_FacturacionFacturaId",
+                table: "DetalleFactura",
+                column: "FacturacionFacturaId");
+
+            migrationBuilder.CreateIndex(
+                name: "IX_DetalleFactura_ProductoId",
+                table: "DetalleFactura",
+                column: "ProductoId");
+
+            migrationBuilder.CreateIndex(
+                name: "IX_Inventario_ProductoId",
+                table: "Inventario",
+                column: "ProductoId");
+        }
+
+        /// <inheritdoc />
+        protected override void Down(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.DropTable(
+                name: "DetalleFactura");
+
+            migrationBuilder.DropTable(
+                name: "Inventario");
+
+            migrationBuilder.DropTable(
+                name: "Facturaciones");
+
+            migrationBuilder.DropTable(
+                name: "Productos");
+        }
+    }
+}
