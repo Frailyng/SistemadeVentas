3da456c1d3e550bd0d8adb1c8440aab82a542a4a
frailyng <frailindes@gmail.com>
2024-10-24 12:52:25 -0400

    Contexto Creado

---
+++ SistemadeVentas/DAL/Contexto.cs
@@ -0,0 +1,15 @@
+﻿using Microsoft.EntityFrameworkCore;
+using SistemadeVentas.Models;
+namespace SistemadeVentas.DAL;
+
+public class Contexto : DbContext
+{
+    public Contexto(DbContextOptions<Contexto> options)
+        : base(options) { }
+
+    public DbSet<Productos> Productos { get; set; }
+
+    public DbSet<Inventario> Inventario { get; set; }
+
+    public DbSet<Facturacion> Facturaciones { get; set; }
+}
